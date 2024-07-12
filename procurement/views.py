dde0d7cf5990320060c228798e8bc767430485bb
Nimantha <nimanthakasun2000@gmail.com>
2024-07-12 12:28:46 +0530

    commit

--- procurement/views.py
+++ procurement/views.py
@@ -3,7 +3,7 @@ from .models import Procurement, Step
 
 def admin_dashboard(request):
     procurements = Procurement.objects.all()
-    return render(request, 'admin_dashboard.html', {'procurements': procurements})
+    return render(request, 'admin_dashboard.html', {'procurements': procurements })
 
 def create_procurement(request):
     if request.method == 'POST':
@@ -16,7 +16,7 @@ def create_procurement(request):
 def view_procurement(request, id):
     procurement = Procurement.objects.get(id=id)
     steps = Step.objects.filter(procurement=procurement).order_by('step_number')
-    return render(request, 'view_procurement.html', {'procurement': procurement, 'steps': steps})
+    return render(request, 'view_procurement.html', {'procurement': procurement, 'steps': steps })
 
 def continue_step(request, id, step):
     procurement = Procurement.objects.get(id=id)
